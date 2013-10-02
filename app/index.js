d097661bcdcde4541c115b608c5f95ffdffab85f
Vilmos Ioo <vilmosi@blinkbox.com>
2013-10-02 14:54:05 +0100

    Added defaults

--- app/index.js
+++ app/index.js
@@ -23,112 +23,190 @@ WordpressThemeGenerator.prototype.askFor = function askFor() {
   // have Yeoman greet the user.
   console.log(this.yeoman);
 
+  // set up defaults, synced with the prompts defaults
+  this.defaults = {
+    author : {
+      name: '',
+      uri: '',
+      email: ''
+    },
+    theme : {
+      name: 'Hyperion',
+      version: '0.0.1',
+      description: 'The Hyperion blank WordPress Theme is a highly customised developer\'s theme, having a lots of features, suitable for a wide range of projects.'
+    },
+    dependencies : {
+      modernizr: true,
+      fontAwesome: true
+    },
+    templates : {
+      customPost: true,
+      metabox: true,
+      settings: true,
+      widget: true,
+      shortcode: true
+    }
+  };
+
   var prompts = [
+    {
+      type: 'confirm',
+      name: 'defaults',
+      message: 'Install default configuration?',
+      default: true
+    },
     {
       type: 'input',
       name: 'authorName',
-      message: 'A few details...\nAuthor name',
-      default: ''
+      message: 'What is your name?',
+      default: this.defaults.author.name,
+      when: function (props) {
+        console.log('\nPlease answer the following:');
+        return !props.defaults;
+      }
     },
     {
       type: 'input',
       name: 'authorUri',
-      message: 'Author URI',
-      default: ''
+      message: 'What is your URI?',
+      default: this.defaults.author.uri,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'input',
       name: 'authorEmail',
-      message: 'Author Email',
-      default: ''
+      message: 'What is your email?',
+      default: this.defaults.author.email,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'input',
       name: 'themeName',
-      message: 'Theme name',
-      default: 'Hyperion'
+      message: 'What is your theme\'s name?',
+      default: this.defaults.theme.name,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'input',
       name: 'themeVersion',
-      message: 'Theme version',
-      default: '0.0.1'
+      message: 'What is your theme\'s version?',
+      default: this.defaults.theme.version,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'input',
       name: 'themeDescription',
-      message: 'Theme description',
-      default: 'The Hyperion blank WordPress Theme is a highly customised developer\'s theme, having a lots of features, suitable for a wide range of projects.'
+      message: 'What is your theme\'s description? (you can edit this later)',
+      default: this.defaults.theme.description,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'confirm',
       name: 'dependenciesModernizr',
       message: 'Do you wish to use Modernizr?',
-      default: true
+      default: this.defaults.dependencies.modernizr,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'confirm',
       name: 'dependenciesFontAwesome',
       message: 'Do you wish to use Font Awesome?',
-      default: true
+      default: this.defaults.dependencies.fontAwesome,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'confirm',
       name: 'templateCustomPost',
-      message: 'Create a custom post template?',
-      default: true
+      message: 'Would you like to include a custom post template?',
+      default: this.defaults.templates.customPost,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'confirm',
       name: 'templateMetabox',
-      message: 'Create a custom metabox?',
-      default: true
+      message: 'Would you like to include a custom metabox?',
+      default: this.defaults.templates.metabox,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'confirm',
       name: 'templateSettings',
-      message: 'Create a theme settings template?',
-      default: true
+      message: 'Would you like to include a theme settings template?',
+      default: this.defaults.templates.settings,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'confirm',
       name: 'templateWidget',
-      message: 'Create a custom widget template?',
-      default: true
+      message: 'Would you like to include a custom widget template?',
+      default: this.defaults.templates.widget,
+      when: function (props) {
+        return !props.defaults;
+      }
     },
     {
       type: 'confirm',
       name: 'templateShortcode',
-      message: 'Create a theme shortcode template?',
-      default: true
+      message: 'Would you like to include a theme shortcode template?',
+      default: this.defaults.templates.shortcode,
+      when: function (props) {
+        return !props.defaults;
+      }
     }
   ];
 
   this.prompt(prompts, function (props) {
-    this.author = {
-      name: props.authorName,
-      uri: props.authorUri,
-      email: props.authorEmail
-    };
-    
-    this.theme = {
-      name: props.themeName,
-      version: props.themeVersion,
-      description: props.themeDescription
-    };
+    if(this.defaults){
+      this.author = this.defaults.author;
+      this.theme = this.defaults.theme;
+      this.dependencies = this.defaults.dependencies;
+      this.templates = this.defaults.templates;
+    } else {
+      this.author = {
+        name: props.authorName,
+        uri: props.authorUri,
+        email: props.authorEmail
+      };
+      
+      this.theme = {
+        name: props.themeName,
+        version: props.themeVersion,
+        description: props.themeDescription
+      };
 
-    this.dependencies = {
-      modernizr: props.dependenciesModernizr,
-      fontAwesome: props.dependenciesFontAwesome
-    };
+      this.dependencies = {
+        modernizr: props.dependenciesModernizr,
+        fontAwesome: props.dependenciesFontAwesome
+      };
 
-    this.templates = {
-      customPost: props.templateCustomPost,
-      metabox: props.templateMetabox,
-      settings: props.templateSettings,
-      widget: props.templateWidget,
-      shortcode: props.templateShortcode
-    };
+      this.templates = {
+        customPost: props.templateCustomPost,
+        metabox: props.templateMetabox,
+        settings: props.templateSettings,
+        widget: props.templateWidget,
+        shortcode: props.templateShortcode
+      };
+    }
 
     cb();
   }.bind(this));
