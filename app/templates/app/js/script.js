e2303fff1fcb1b7a44e6edc236133abcb66dd336
Vilmos Ioo <cool.villi@gmail.com>
2013-10-15 01:07:04 +0100

    Removed plugins.js, updated script.js and wp-tools.

--- app/templates/app/js/script.js
+++ app/templates/app/js/script.js
@@ -1,6 +1,26 @@
 'use strict';
 
+/*
+* on document load script 
+*/
 jQuery(document).ready(function() {
 
-
 });
+
+/*
+* Basic JS module pattern
+*/
+var APP = (function (app, $) {
+
+	app.init = function(){
+		var i = 0;
+		console.log(i);
+		// add some code
+	}
+
+	// add more code here
+
+	return app;
+}(APP || {}, jQuery));
+
+jQuery(document).ready(APP.init);
