cc69f5659edcfbdb0cc9aaeb664db836833c79ee
Vilmos Ioo <cool.villi@gmail.com>
2013-12-27 01:59:05 +0000

    Update test

--- test/test-creation.js
+++ test/test-creation.js
@@ -6,7 +6,7 @@ var helpers = require('yeoman-generator').test;
 
 
 describe('wordpress-theme generator', function () {
-  beforeEach(function (done) {
+  beforeEach(function (done) {    
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
         return done(err);
@@ -22,12 +22,32 @@ describe('wordpress-theme generator', function () {
   it('creates expected files', function (done) {
     var expected = [
       // add files you expect to exist here.
+      'package.json',
+      'Gruntfile.js',
+      'README.md',
+      'bower.json',
       '.jshintrc',
-      '.editorconfig'
+      '.gitignore',
+      '.bowerrc',
+      'app/404.php', 
+      'app/archive.php', 
+      'app/comments.php', 
+      'app/footer.php', 
+      'app/front-page.php', 
+      'app/functions.php', 
+      'app/header.php', 
+      'app/index.php', 
+      'app/page.php', 
+      'app/screenshot.png', 
+      'app/search.php', 
+      'app/searchform.php', 
+      'app/sidebar.php', 
+      'app/single.php', 
+      'app/style.scss'  
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': true
+      'defaults': true
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
