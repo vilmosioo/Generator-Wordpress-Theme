b4134ddc36bdd726d30e1d32800cdc93d95f1a3a
Vilmos Ioo <cool.villi@gmail.com>
2013-12-29 18:00:25 +0000

    Added another template

--- app/templates/app/search.php
+++ app/templates/app/search.php
@@ -11,11 +11,7 @@ get_header();
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 		<?php get_template_part( 'content', get_post_format() ); ?>
 	<?php endwhile; ?>
-	<aside class='aside' id='post-navigation'>
-		<span class='fleft'><?php previous_posts_link(); ?></span> 
-		<span class='fright'><?php next_posts_link(); ?></span> 
-		<div class='clear'></div>
-	</aside>
+	<?php get_template_part( 'templates/posts-navigation' ); ?>
 </section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
---
+++ app/templates/app/templates/posts-navigation.php
@@ -0,0 +1,5 @@
+<aside class='aside'>
+	<span class='fleft'><?php previous_posts_link(); ?></span> 
+	<span class='fright'><?php next_posts_link(); ?></span> 
+	<div class='clear'></div>
+</aside>
